a0f3cbb47463a2c623b9da05cefdabc2560f4fd3
Advik-Gupta <advikguptadev@gmail.com>
2025-06-28 23:24:56 +0530

    allowed cors localhost policy for testing

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import cv2
 from utils.processor import correct_image_orientation
 import io
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",  # just in case
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/process-image/")
 async def process_image(file: UploadFile = File(...)):
