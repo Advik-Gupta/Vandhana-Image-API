97dda8a23d01f8e61e20695113d875dff995ef22
Advik-Gupta <advikguptadev@gmail.com>
2025-07-25 15:23:23 +0530

    deployed on render

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ import io
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",  # just in case
-]
+# origins = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "https://vandhana-portal-frontend.vercel.app/"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
